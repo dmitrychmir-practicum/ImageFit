8353589f87e9aeaca90e704c64cc717c2065252f
Dmitry V. Chmir <dmitrychmir@yandex.ru>
2025-10-06 13:44:57 +0300

    Финал по 8 спринту

---
+++ ImageFit/Extensions/ImagesListViewController+Extensions.swift
@@ -0,0 +1,44 @@
+//
+//  ImagesListViewController+Extensions.swift
+//  ImageFit
+//
+//  Created by Дмитрий Чмир on 05.10.2025.
+//
+
+import UIKit
+
+extension ImagesListViewController:  UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photosName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imagesListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        configCell(for: imagesListCell, with: indexPath)
+        return imagesListCell
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+        
+        let insets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let width = tableView.bounds.width - insets.left - insets.right
+        let scale = width / image.size.width
+        let height = image.size.height * scale + image.capInsets.top + image.capInsets.bottom
+        
+        return height
+    }
+}
---
+++ ImageFit/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  ImageFit
+//
+//  Created by Дмитрий Чмир on 04.10.2025.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet
+    private var tableWiew: UITableView!
+    
+    let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        formatter.locale = Locale(identifier: "ru_RU")
+        return formatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableWiew.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath){
+        guard let image = UIImage(named: photosName[indexPath.row]) else { return }
+
+        cell.dataLabel.text = dateFormatter.string(from: Date())
+        cell.cellImage.image = image
+        cell.cellImage.layer.cornerRadius = 16
+        cell.cellImage.layer.masksToBounds = true
+        
+        let isActive = indexPath.row % 2 == 0
+        let likeImage = isActive ? UIImage(named: "Active") : UIImage(named: "NoActive")
+        
+        cell.likeButton.setImage(likeImage, for: .normal)
+    }
+}
+
